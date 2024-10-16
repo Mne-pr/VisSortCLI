785a337ae2d89bc8f0936da118bca48add4df94f
Mne-pr <thousand2000@naver.com>
2024-10-17 00:19:14 +0900

    selectionSort class added

--- sort.cpp
+++ sort.cpp
@@ -16,6 +16,7 @@ class printer {
     int m_value = 0;
     int cur1 = 0;
     int cur2 = 0;
+    int tmp = 0;
 
 public:
     printer(int sizex, char* winName, int tickt) : ticktime(tickt) {
@@ -27,6 +28,7 @@ public:
     }
     ~printer() { delete(panel); return; }
 
+    // 처음에 버퍼 설정
     void insertData(const vector<int>& data) {
 
         int idx = 2; // 2 ~ 가로길이-2
@@ -56,7 +58,8 @@ public:
 
         return;
     }
-
+    
+    // 두 값 위치 변경
     void printChange(const vector<int> res) {
 
         // up
@@ -99,8 +102,9 @@ public:
         return;
     }
 
+    // 커서 출력 - 커서가 2개인 것으로 한정했는데 확장가능성 있음
     void printCursor(int idx1, int idx2) {
-        return;
+
         for (int i = 0; i < 2; i++) {
             panel->screenRender(0, 0, L'M');
             panel->screenRender(6 + cur1 * 2, 1, L' ');
@@ -115,6 +119,7 @@ public:
         return;
     }
 
+    // 정렬 완료 후
     void endPrint() {
         panel->screenClear(0, 0, 20, 0, true);
         panel->screenRender(0, 0, L"Sorting complete");
@@ -123,35 +128,51 @@ public:
         Sleep(ticktime * 20); // 사용자 입력 받아서 종료한다던가
         return;
     }
+
+
+    //
+
+
+    // 두 끝 인덱스의 값 슬라이드
+    void printSlide(int idx1, int idx2, int direction) {
+
+    }
+
+    // 특정 값 임시저장(프린터 제거)
+    void saveTemp() {
+
+    }
 };
 
 // 정렬 계산
 class allSort {
+protected:
+    vector<int>* data = nullptr;
+    int cycle = 0;
+    int max_v = 0;
+    int min_v = 0;
+
 public:
     allSort() {}
     ~allSort() {}
-    virtual vector<int> go1step() = 0; // 추상 클래스임을 명확히 하는 순수가상함수
-    virtual int getDatalen() = 0;
-    virtual void insertData(vector<int>* newdata) = 0;
-};
 
-// 정렬 : 버블 정렬
-class bubbleSort : public allSort {
-    vector<int>* data = nullptr;
-    int cycle = 0;
-    int cur = 0;
-
-public:
-    bubbleSort() { return; }
-    ~bubbleSort() { return; }
-
-    void insertData(vector<int>* newdata) override {
+    void insertData(vector<int>* newdata) {
         data = newdata;
+        max_v = getMaxValue();
+        min_v = getMinValue();
         return;
     }
 
-    int getDatalen() override { 
-        return data ? data->size() : 0; 
+    int getDatalen() {
+        return data ? data->size() : 0;
+    }
+
+    int getMaxValue() {
+        return data ? *max_element(data->begin(), data->end()) : 0;
+    }
+
+    int getMinValue() {
+        return data ? *min_element(data->begin(), data->end()) : 0;
     }
 
     void vchange(int idx1, int idx2) {
@@ -160,6 +181,17 @@ public:
         (*data)[idx2] = tmp;
         return;
     }
+
+    virtual vector<int> go1step() = 0; // 추상 클래스임을 명확히 하는 순수가상함수
+
+};
+
+// 정렬 : 최소 버블 정렬 (오름차순)
+class bubbleSort : public allSort {
+    int cur = 0;
+public:
+    bubbleSort() { return; }
+    ~bubbleSort() { return; }
 
     vector<int> go1step() override {
         // 변화유무, 인덱스1, 인덱스2, 커서1, 커서2
@@ -179,6 +211,40 @@ public:
 
         // 다음 사이클로 넘어가면 되나요?
         else { cycle += 1; cur = 0; }
+
+        return foret;
+    }
+};
+
+// 정렬 : 최소 선택 정렬 (오름차순)
+class selectionSort : public allSort {
+    int cur1 = 0;
+    int cur2 = 1;
+    int minidx = 0;
+
+public:
+    selectionSort() { return; }
+    ~selectionSort() { return; }
+
+    vector<int> go1step() override {
+        // 변화유무, 인덱스1, 인덱스2, 커서1, 커서2
+        vector<int> foret = { 0,0,0,0,0,cur1,cur2 };
+
+        // 해당 인덱스와 minidx 값을 비교하고, 다음 커서로 넘어갈까요?
+        if (cur2 < data->size()) {
+            if ((*data)[cur2] < (*data)[minidx]) { minidx = cur2; }
+            cur2 += 1; 
+        }
+
+        // 정렬이 끝났나요?
+        else if (cur1 == data->size() - 1) { foret = { -1,0,0,0,0,cur1 - 1,cur1 }; }
+
+        // 다음 사이클로 넘어가면 되나요? - 값 갱신
+        else {
+            foret = { 1,cur1,(*data)[cur1],minidx,(*data)[minidx],cur1,minidx };
+            vchange(cur1, minidx); 
+            cycle += 1; cur1 += 1; cur2 = cur1 + 1; minidx = cur1;
+        }
 
         return foret;
     }
@@ -223,8 +289,9 @@ int main() {
     cout << "정렬할 데이터 개수를 입력해주세요. > ";
     cin >> datalen;
 
-    // 정렬 방식은 bubbleSort 설정
-    bubbleSort method;
+    // 정렬 방법 객체 생성
+    //bubbleSort method;
+    selectionSort method;
 
     // 입력받은 데이터 수 만큼 랜덤 생성
     vector<int> data(datalen);
